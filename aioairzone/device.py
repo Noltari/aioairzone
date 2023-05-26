b1e048a417b67bfbdc21c439af6b6721b5702e9c
Álvaro Fernández Rojas <noltari@gmail.com>
2023-05-26 11:09:46 +0200

    thermostat: move to its own file
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- aioairzone/device.py
+++ aioairzone/device.py
@@ -13,7 +13,6 @@ from .common import (
     SleepTimeout,
     SystemType,
     TemperatureUnit,
-    ThermostatType,
 )
 from .const import (
     API_AIR_DEMAND,
@@ -58,9 +57,6 @@ from .const import (
     API_SYSTEM_ID,
     API_SYSTEM_TYPE,
     API_TEMP_STEP,
-    API_THERMOS_FIRMWARE,
-    API_THERMOS_RADIO,
-    API_THERMOS_TYPE,
     API_UNITS,
     API_ZONE_ID,
     AZD_ABS_TEMP_MAX,
@@ -121,10 +117,9 @@ from .const import (
     DEFAULT_TEMP_STEP_FAHRENHEIT,
     ERROR_SYSTEM,
     ERROR_ZONE,
-    THERMOSTAT_RADIO,
-    THERMOSTAT_WIRED,
 )
 from .exceptions import InvalidZone
+from .thermostat import Thermostat
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -325,44 +320,6 @@ class System:
 
         if API_SYSTEM_TYPE in data:
             self.type = SystemType(data[API_SYSTEM_TYPE])
-
-
-class Thermostat:
-    """Airzone Thermostat."""
-
-    def __init__(self, data: dict[str, Any]):
-        """Thermostat init."""
-        self.firmware: str | None = None
-        self.radio: bool | None = None
-        self.type: ThermostatType | None = None
-
-        if API_THERMOS_FIRMWARE in data:
-            self.firmware = str(data[API_THERMOS_FIRMWARE])
-        if API_THERMOS_RADIO in data:
-            self.radio = bool(data[API_THERMOS_RADIO])
-        if API_THERMOS_TYPE in data:
-            self.type = ThermostatType(data[API_THERMOS_TYPE])
-
-    def get_firmware(self) -> str | None:
-        """Return Airzone Thermostat firmware."""
-        if self.firmware and "." not in self.firmware and len(self.firmware) > 2:
-            return f"{self.firmware[0:1]}.{self.firmware[1:]}"
-        return self.firmware
-
-    def get_model(self) -> str | None:
-        """Return Airzone Thermostat model."""
-        if self.type:
-            name = str(self.type)
-            if self.type.exists_radio():
-                sfx = f" ({THERMOSTAT_RADIO if self.radio else THERMOSTAT_WIRED})"
-            else:
-                sfx = ""
-            return f"{name}{sfx}"
-        return None
-
-    def get_radio(self) -> bool | None:
-        """Return Airzone Thermostat radio."""
-        return self.radio
 
 
 class Zone:
